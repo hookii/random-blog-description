a6c5f9ff36a14c7f3aeca70c900c15bbf7070e55
hg <crazyhg+git@gmail.com>
2018-05-18 23:49:54 +0200

    Original code

---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Random Blog Description ===
+Contributors: rndz
+Donate link: http://rndz.org/
+Tags: random, blog, description, blogdescription, 
+Requires at least: 2.0.2
+Tested up to: 3.1.4
+Stable tag: 1.0
+
+Here is a short description of the plugin.  This should be no more than 150 characters.  No markup here.
+
+== Description ==
+
+This is a plugin for random description,that is select a sentence with random in you like.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `rndesc.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place sentences for you like in `rndesc.php.lst`
+3. Go to the Plugins -> Random Blog Description, Place sentences
+
+== Frequently Asked Questions ==
+
+= I have not received any issues =
+So it is no answer
+
+== Screenshots ==
+
+1. This is screen shot for Random Blog Description Setting
+
+== Changelog ==
+
+= 1.0 =
+First release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+First release.
---
+++ rndesc.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Plugin Name: Random Blog Description
+Description: This is a plugin for random description,that is select a sentence with random in you like.
+Author: z
+Version: 1.0
+Author URI: http://rndz.org/
+*/
+
+define('RNDESC',__FILE__.'.lst');
+
+function random_description($opt) {
+	static $i;
+	$str = file(RNDESC);
+	if(!isset($i)){
+		$i = array_rand($str);
+	}
+	return trim($str[$i]);
+}
+
+function random_description_footer(){
+	echo '<script type="text/javascript">(function(){document.getElementById("site-description").innerHTML="'.random_description('').'";})();</script>';
+}
+
+function random_description_option_form(){
+	global $title;
+	echo '<br>';
+	if($_POST['sentence']){
+		update_option('random_description_format',$_POST['format']);
+		file_put_contents(RNDESC,get_magic_quotes_gpc()?stripslashes($_POST['sentence']):$_POST['sentence']);
+		echo '<div class="wrap"><div id="setting-error-settings_updated" class="updated settings-error"><p><strong>';
+		echo __('Settings saved.');
+		echo "</strong></p></div></div>";
+	}
+	echo '<div id="poststuff"><div class="postbox">';
+	echo "<h3>$title</h3>";
+	echo '<div class="inside less"><form method="post"><h4>'.__('You like sentences').'</h4><textarea name="sentence" style="width:100%;height:200px;" wrap="off">';
+	readfile(RNDESC);
+	echo '</textarea><h4>'.__('What is output format').'</h4>';
+	foreach(array('Javascript','Direct') as $k => $v){
+		echo '<label><input name="format" type="radio" value="'.$k.($k==get_option('random_description_format')?'" checked':'"')." />$v</label><br />";
+	}
+	echo '<br /><input type="submit" value="'.__('Save').'" class="button-primary" /></form></div></div></div>';
+}
+
+function random_description_admin_menu(){
+	add_submenu_page('plugins.php',__('Random Blog Description Settings'),__('Random Blog Description'),8,'RandomDescription',random_description_option_form);
+}
+
+function random_description_meta($links,$file){
+	if($file==basename(__FILE__)){
+		$links[] = '<a href="plugins.php?page=RandomDescription">' . __('Settings') . '</a>';
+	}
+	return $links;
+}
+
+function random_description_deactivation(){
+	delete_option('random_description_format');
+}
+
+function random_description_activation(){
+	if(!file_exists(RNDESC)){
+		file_put_contents(RNDESC,__('That is a random description'));
+		touch(RNDESC);
+	}
+}
+
+add_action('admin_menu',random_description_admin_menu);
+add_filter('plugin_row_meta',random_description_meta,10,2);
+register_activation_hook(__FILE__, random_description_activation);
+register_deactivation_hook(__FILE__,random_description_deactivation);
+
+if(is_admin()){
+}elseif(get_option('random_description_format')){
+	add_filter('pre_option_blogdescription',random_description);
+}else{
+	add_action('wp_footer',random_description_footer);
+}
